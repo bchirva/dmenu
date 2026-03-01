39d9dbc8b4c9cd130bec6667d673f0fcac7b4475
b_chirva <bchirva@gmail.com>
2026-03-02 00:14:34 +0500

    Add padding patch

--- config.def.h
+++ config.def.h
@@ -4,6 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+static int horizpadbar = 2;                 /* horizontal padding */
+static int vertpadbar = 16;                  /* vertical padding */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char *fonts[] = {
