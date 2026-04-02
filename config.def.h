6ee7cc5b3c2164bf1332e122deea16a90cca21ad
b_chirva <bchirva@gmail.com>
2026-04-02 18:25:06 +0500

    Add highlight patch

--- config.def.h
+++ config.def.h
@@ -17,6 +17,7 @@ static char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeIn] = { "#333333", "#eeeeee" },
+	[SchemeHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
